9346b281ca62bd0667a3d27971b815c9a1bf8166
princejoogie <princejoogie@gmail.com>
2022-10-12 01:32:40 +0800

    chore: create vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,25 @@ provider "aws" {
 }
 
 # Creating an EC2 instance
-resource "aws_instance" "test-server" {
-  ami           = "ami-07651f0c4c315a529"
-  instance_type = "t2.micro"
+# resource "aws_instance" "test-server" {
+#   ami           = "ami-07651f0c4c315a529"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "ubuntu-server"
+#   }
+# }
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "ubuntu-server"
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "prod-subnet"
   }
 }
